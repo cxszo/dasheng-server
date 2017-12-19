7b8f322c3ff941ed1249fcbba69d74304493cf4a
王炜 <1617634@qq.com>
2017-12-19 19:42:30 +0800

    贷款后台接口

--- interface/admin.js
+++ interface/admin.js
@@ -1,13 +1,14 @@
 var express = require('express');
+var fs = require('fs')
+var path = require('path')
 var app = express();
 var router = express.Router();
 var User = require('../models/user')
+var util = require('../util/')
 
 //查看注册用户列表
 router.get('/userlist', (req, res)=>{
     User.findUserList((err, _user)=>{
-        
-        
         
         let __user = _user.map((item)=>{
             let { user_id, callphone, username, meta } = item
@@ -27,5 +28,57 @@ router.get('/userlist', (req, res)=>{
     })
 })
 
+//贷款运营-冯竹君
+/**
+ * ps        一页显示多少条数据 默认不传 1000
+ * pn        当前第几页 默认查第一页
+ * startDate 查询的起始时间 默认上一个月 非自然月
+ * endDate   查询的截止时间 默认今天
+ */
+router.get('/zhuj', (req, res)=>{
+  let { startDate:start, endDate:end, ps='1000', pn='1' } = req.query;
+
+  let filePath = path.resolve(process.cwd(), '../dasheng/admin/data/data.json');
+  let desc = '查询成功'
+  let result = [], data=[];
+  try{
+    result = fs.readFileSync(filePath, 'utf-8');
+    result = JSON.parse(result).reverse();
+
+    if(result.length){
+      start = start || '';
+      if(util.isDate(start)){
+        start = new Date(start).getTime()
+      }else{
+        start = new Date().setMonth(new Date().getMonth()-1)//默认查 非自然月一个月的数据
+      }
+      end = end || '';
+      if(util.isDate(end)){
+        end = new Date(end).getTime()
+      }else{
+        end = new Date().getTime()
+      }
+      result = result.filter((v)=>{
+        let { start_date, end_date } = v;
+        return new Date(start_date).getTime() >= start && new Date(end_date).getTime() <= end
+      })
+      data = result.slice(ps*(pn-1), ps*pn);
+      desc = data.length? desc : '当前页没有数据'
+    }
+  }catch(e){
+    desc = '查询失败'
+  }
+
+  res.contentType('json');
+  res.send({
+      code:'1',
+      data,
+      ps,
+      pn,
+      total: result.length,
+      desc
+  });
+  return false;
+})
 
 module.exports = router;
--- util/index.js
+++ util/index.js
@@ -18,7 +18,30 @@ module.exports = {
         if(!data.length)
             return -2;
         return 1;
-    }
+    },
+    /**
+     * 判断传入的值是否是时间字符串 
+     * @param {String} dateString 日期字符串
+     * example:
+     * 2017/12/19  > true
+     * 2017-12-19  > true
+     * else  > false
+     */
+    isDate(dateString){
+      if(dateString.trim()==""){
+        return false;
+      }
+      var r=dateString.match(/^(\d{1,4})(-|\/)(\d{1,2})\2(\d{1,2})$/); 
+      if(r==null){
+        return false;
+      }
+      var d=new Date(r[1],r[3]-1,r[4]);   
+      var num = (d.getFullYear()==r[1]&&(d.getMonth()+1)==r[3]&&d.getDate()==r[4]);
+      if(num==0){
+      }
+      return (num!=0);
+   } 
+  
 
 
 }
